54b3a01d8f0c5e0ee9f8369be59222465d3bb11e
RaphelLesourd <birkyboy@icloud.com>
2026-02-20 11:07:37 +0100

    chore: bootstrap macOS menu bar project

---
+++ Codex rate limit/Codex_rate_limitApp.swift
@@ -0,0 +1,28 @@
+import AppKit
+import SwiftUI
+
+@main
+struct CodexRateLimitApp: App {
+  @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+  @StateObject private var viewModel: RateLimitViewModel
+
+  init() {
+    let environment = AppEnvironment.live()
+    _viewModel = StateObject(wrappedValue: RateLimitViewModel(environment: environment))
+  }
+
+  var body: some Scene {
+    MenuBarExtra {
+      MainMenuView(viewModel: viewModel)
+    } label: {
+      MenuBarLabelView(viewModel: viewModel)
+    }
+    .menuBarExtraStyle(.window)
+  }
+}
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    NSApp.setActivationPolicy(.accessory)
+  }
+}
